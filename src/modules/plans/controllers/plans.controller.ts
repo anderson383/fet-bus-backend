964d61f7ee5550ba532e26499e6a706743cbfffb
juanCamilo2002 <juan2002djfh@gmail.com>
2024-09-17 11:27:08 -0500

    feat: agrega doc summary swagger

--- src/modules/plans/controllers/plans.controller.ts
+++ src/modules/plans/controllers/plans.controller.ts
@@ -4,35 +4,52 @@ import { CreatePlanDto } from '../dto/create-plan.dto';
 import { UpdatePlanDto } from '../dto/update-plan.dto';
 import { AuthGuard } from 'src/modules/auth/guards/auth.guard';
 import { AdminGuard } from 'src/modules/auth/guards/admin.guard';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('plans')
+@ApiTags('Controlador de planes')
 @UseGuards(AuthGuard, AdminGuard)
 export class PlansController {
   constructor(private readonly plansService: PlansService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Crear un nuevo plan.',
+  })
   create(@Body() createPlanDto: CreatePlanDto, @Req() req) {
     this.plansService.userId = req.user.id;
     return this.plansService.create(createPlanDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Obtener todos los planes.',
+  })
   findAll() {
     return this.plansService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Obtener un plan por su id.',
+  })
   findOne(@Param('id') id: string) {
     return this.plansService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Actualizar un plan por su id.',
+  })
   update(@Param('id') id: string, @Body() updatePlanDto: UpdatePlanDto, @Req() req) {
     this.plansService.userId = req.user.id;
     return this.plansService.update(id, updatePlanDto);
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Eliminar un plan por su id.',
+  })
   remove(@Param('id') id: string, @Req() req) {
     this.plansService.userId = req.user.id;
     return this.plansService.remove(id);
--- src/modules/plans/dto/create-plan.dto.ts
+++ src/modules/plans/dto/create-plan.dto.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString, IsNumber } from "class-validator";
 
 
 export class CreatePlanDto {
     @IsString()
+    @ApiProperty({ description: 'Nombre del plan', example: 'Plan A' })
     name: string;
 
     @IsNumber()
+    @ApiProperty({ description: 'Valor por dia', example: 10000 })
     amount_perday: number;
 
     @IsNumber()
-    equals_day: number;    
+    @ApiProperty({ description: 'Cantidad de dias', example: 30 })
+    equals_day: number;
 }
